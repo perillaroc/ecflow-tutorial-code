2eef178afc42c20354b48eb486cb3d19a1ba6239
perillaroc <perillaroc@gmail.com>
2018-01-31 20:28:01 +0800

    add code for add another task for chapter 03.

--- src/chap03/01_add_another_task/test.py
+++ src/chap03/01_add_another_task/test.py
@@ -1,13 +1,17 @@
 import os
 from pathlib import Path
-from ecflow import Defs, Suite, Task, Edit, Client
+from ecflow import Defs, Suite, Task, Edit
 
 print("Creating suite definition")
 home = os.path.abspath(Path(Path(__file__).parent, "../../../build/course"))
 defs = Defs(
-    Suite('test',
-          Edit(ECF_HOME=home),
-          Task('t1')))
+    Suite(
+        'test',
+        Edit(ECF_HOME=home),
+        Task('t1'),
+        Task('t2')
+    )
+)
 print(defs)
 
 print("Checking job creation: .ecf -> .job0")
@@ -15,10 +19,3 @@ print(defs.check_job_creation())
 
 print("Saving definition to file 'test.def'")
 defs.save_as_defs(str(Path(home, "test.def")))
-
-try:
-    print("Load the in memory definition(defs) into the server")
-    ci = Client('login05', '33083')
-    ci.load(defs)
-except RuntimeError as e:
-    print("Failed:", e)
--- src/chap03/01_add_another_task/test_client.py
+++ src/chap03/01_add_another_task/test_client.py
@@ -6,6 +6,8 @@ home = os.path.abspath(Path(Path(__file__).parent, "../../../build/course"))
 try:
     print("Loading definition in 'test.def' into the server")
     ci = ecflow.Client('login05', '33083')
-    ci.load(str(Path(home, "test.def")))  # read definition from disk and load into the server
+    ci.delete("/test")
+    ci.load(str(Path(home, "test.def")))
+    ci.begin_suite("test")
 except RuntimeError as e:
     print("Failed:", e)
