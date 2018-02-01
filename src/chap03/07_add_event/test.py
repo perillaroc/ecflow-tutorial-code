cc69c1c732137bde3323778b385787b258808979
perillaroc <perillaroc@gmail.com>
2018-02-01 11:23:15 +0800

    add code for add event section.

---
+++ src/chap03/07_add_event/test.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+from ecflow import Defs, Suite, Task, Family, Edit, Trigger, Event
+
+
+def create_family_f1():
+    return Family(
+        "f1",
+        Edit(SLEEP=20),
+        Task("t1"),
+        Task(
+            "t2",
+            Trigger("t1 == complete"),
+            Event('a'),
+            Event('b')),
+        Task(
+            "t3",
+            Trigger("t2:a")),
+        Task(
+            "t4",
+            Trigger("t2:b"))
+    )
+
+
+print("Creating suite definition")
+home = os.path.abspath(Path(Path(__file__).parent, "../../../build/course"))
+defs = Defs(
+    Suite('test',
+          Edit(ECF_INCLUDE=home, ECF_HOME=home),
+          create_family_f1()))
+print(defs)
+
+print("Checking job creation: .ecf -> .job0")
+print(defs.check_job_creation())
+
+print("Saving definition to file 'test.def'")
+defs.save_as_defs(str(Path(home, "test.def")))
+
+# To restore the definition from file 'test.def' we can use:
+# restored_defs = ecflow.Defs("test.def")
